82c0eeaf5d0965f82bcf8647682485a8b541f94d
Jonas Elvedal Hole <jolejonaseh@gmail.com>
2020-11-14 17:52:13 +0100

    Revert "Label some colors"
    This reverts commit 99193035bb29428c518e55fb153187d279299985.

--- config.def.h
+++ config.def.h
@@ -107,19 +107,19 @@ static const char *colorname[] = {
 
 	/* 8 bright colors */
 	"#4c566a", /* black   */
-	"#bf616a", /* red     */	// used in ls for .gz
-	"#a3be8c", /* green   */	// used in ls for executable
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
 	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */	// used in ls for /
-	"#b48ead", /* magenta */	// used in ls for media (png,mkv,mp4,...)
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
 	"#8fbcbb", /* cyan    */
 	"#eceff4", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#2e3440", /* background */	// the background
-	"#d8dee9", /* foreground */	// used for plain-text
+	"#2e3440", /* background */
+	"#d8dee9", /* foreground */
 };
 
 
